aa6222e122637c1e0b21e660388e4db50fff1969
Marcus Hultkrantz <marcus.hultkrantz@isotop.se>
2019-11-15 14:46:26 +0100

    Updates

---
+++ precache-manifest.69cd0764d680c1cc2c333c1ff073f59a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44c833ad1ca94b484a65e3d4446dbd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cea7e373e44ea5f6799d",
+    "url": "/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "a3663297f8faba526153",
+    "url": "/static/js/2.b736c94c.chunk.js"
+  },
+  {
+    "revision": "cea7e373e44ea5f6799d",
+    "url": "/static/js/main.58b94bb5.chunk.js"
+  },
+  {
+    "revision": "36523bc08e881f073c96",
+    "url": "/static/js/runtime-main.2ddccb8d.js"
+  }
+]);
